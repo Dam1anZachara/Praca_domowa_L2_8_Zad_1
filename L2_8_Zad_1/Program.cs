dfc4122c45e5d36562a52140edbf29d904f9bf60
Damian Z <damian9203@gmail.com>
2021-12-19 12:13:59 +0100

    Add project files.

---
+++ L2_8_Zad_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace L2_8_Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = 0;
+            int endNumber = 100;
+            int primeNumberCounter = 0;
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                int numberOfDivisors = 0;
+                //Console.WriteLine("Nr " + i);
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        numberOfDivisors++; 
+                    }
+                }
+                if (numberOfDivisors == 2)
+                {
+                    primeNumberCounter++;
+                    Console.WriteLine("Lp.: " + i);
+                }
+            }
+            Console.WriteLine($"W zakresie liczb {startNumber} - {endNumber} jest {primeNumberCounter} liczb pierwszych ");
+        }
+    }
+}
